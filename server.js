6e875bd8162126f5abd8f3bf1afea62b61c95e23
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-27 18:01:49 +0530

    chore(server): Update Express server configuration
    - Updated the Express server configuration to include new routes and reorganize existing ones.
    - Added new routes for user-related functionalities and comments.
    - Organized route imports for better readability.
    - Removed unnecessary route paths.
    - Ensured that all route paths are versioned under '/v1/api'.
    - Kept existing routes for authentication and posts.
    - Updated server initialization message.
    
    This commit refactors and expands the Express server to accommodate new routes and maintain a well-structured codebase.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+
+
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
+const tagRoutes = require('./routes/tagRoutes');
+const commentRoute = require('./routes/commentRoute');
+
+const userRoutes = require('./routes/User Routes/userRoute');
+const userRoleRoutes = require('./routes/User Routes/userRoleRoute');
+
 const cors = require('cors');
 
 dotenv.config();
@@ -13,7 +21,15 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/auth', authRoutes);
-app.use('/post', postRoutes)
+app.use('/v1/api/posts', postRoutes);
+
+app.use('/v1/api/users', userRoutes);
+app.use('/v1/api/users/role', userRoleRoutes);
+
+app.use('/v1/api/tags', tagRoutes);
+
+app.use('/v1/api/comments', commentRoute);
+
 
 app.get('/', (req, res) => {
   res.send("Welcome to the SphereNet Backend Server");
