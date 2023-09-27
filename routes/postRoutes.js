8ad69a023e156f430041f9205d9a5bb2d4a6c263
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-27 20:55:33 +0530

    feat(routes): Update Post routes
    - Added routes for creating a post, getting all posts, getting a post by ID, deleting a post, liking and unliking a post, adding comments to a post, getting comments on a post, and deleting a comment on a post.
    - Included middleware for requiring sign-in to perform certain actions and checking if a user is an admin.
    - Organized the routes for better readability.
    
    This commit updates the Post routes to include various post-related actions and adds necessary middleware for authentication and authorization.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,37 @@
 const express = require('express');
+
 const { 
   createPost, 
+  getAllPosts,
+  deletePost,
+  likePost,
+  unlikePost,
+  getPostById,
+  addCommentsToPostsById,
+  getCommentsOnPostById,
+  deleteCommentOnPostById,
 } = require('../controllers/postController');
+
+const { 
+  requireSignIn ,
+  isAdmin
+} = require('../middlewares/authMiddleware');
+
 
 const router = express.Router();
 
-router.post('/create', createPost)
+router.post('/', requireSignIn, createPost);
+
+router.get('/', requireSignIn, getAllPosts);
+router.get('/:postId', requireSignIn, getPostById);
+
+router.delete('/delete-post/:postId' , deletePost)
+
+router.put('/like/:postId', requireSignIn, likePost);
+router.put('/dislike/:postId', requireSignIn, unlikePost);
+
+router.put('/comments/:postId', requireSignIn, addCommentsToPostsById);
+router.get('/comments/:postId', requireSignIn, getCommentsOnPostById);
+router.patch('/comments/:postId', requireSignIn, deleteCommentOnPostById);
 
 module.exports = router;
