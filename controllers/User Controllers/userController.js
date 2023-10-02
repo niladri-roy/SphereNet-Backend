49b2f3de39743338c88d738c38563a59b98f311b
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-10-02 21:41:11 +0530

    feat: Implement user retrieval and update endpoints
    - Add user retrieval endpoint by ID
    - Implement user details update functionality
    - Handle user not found and error cases
    
    This commit adds new functionality to retrieve user details by ID and update user information, including first name, last name, location, date of birth, gender, headline, and bio. It also includes error handling and validation to ensure data integrity.

--- controllers/User Controllers/userController.js
+++ controllers/User Controllers/userController.js
@@ -27,6 +27,63 @@ const getUsersById = async (req , res) => {
   }
 }
 
+const updateUsersById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+
+    const { firstName, lastName, location, dateOfBirth, gender, headline, bio } = req.body;
+
+    // Update user's details if provided in the request
+    if (firstName) {
+      user.firstName = firstName;
+    }
+    if (lastName) {
+      user.lastName = lastName;
+    }
+    if (location) {
+      user.location = location;
+    }
+    if (dateOfBirth) {
+      user.dateOfBirth = dateOfBirth;
+    }
+    if (gender) {
+      user.gender = gender;
+    }
+    if (headline) {
+      user.headline = headline;
+    }
+    if (bio) {
+      user.bio = bio;
+    }
+
+    // Save the updated user
+    await user.save();
+
+    res.status(200).send({
+      success: true,
+      message: "User Details Updated Successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Updating User's Details -> Internal Server Error",
+      error: error,
+    });
+  }
+};
+
+
 module.exports = {
-  getUsersById
+  getUsersById,
+  updateUsersById
 };
